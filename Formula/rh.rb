4631115970f92d515570d0b0bbfb5c06ad7a20e2
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-05-31 18:46:36 -1000

    Brew formula update for request_hole version v0.5.1

--- Formula/rh.rb
+++ Formula/rh.rb
@@ -5,25 +5,25 @@
 class Rh < Formula
   desc "Request Hole is a command line tool for creating a temporary endpoint."
   homepage "https://github.com/aaronvb/request_hole"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.0/request_hole_0.5.0_Darwin_x86_64.tar.gz"
-    sha256 "d41a9acae06a5887214435474180c869b9fc47e5e415964565238645a19e58e1"
+    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.1/request_hole_0.5.1_Darwin_x86_64.tar.gz"
+    sha256 "1dc972d7dce6e8af9e8055adef037f8f0923702bffd00e72b4a79194ca8d3d1a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.0/request_hole_0.5.0_Darwin_arm64.tar.gz"
-    sha256 "f5cef6b99a8b4ac0613832afccd0fd4f767449639f40ec5f1205a9facb6ac335"
+    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.1/request_hole_0.5.1_Darwin_arm64.tar.gz"
+    sha256 "b6d9edd81296572e83363dd63645846e82a90c333927a35a477b15e999ace164"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.0/request_hole_0.5.0_Linux_x86_64.tar.gz"
-    sha256 "6941f466573b9456f365685b855a320b78b4602a508c180232c9e2dc556b81fb"
+    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.1/request_hole_0.5.1_Linux_x86_64.tar.gz"
+    sha256 "c32be5b691e7f59ca5bd26e7b85fb75d862510af4597d0d03c132d00c45a3cb2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.0/request_hole_0.5.0_Linux_arm64.tar.gz"
-    sha256 "c87392f8fecc1b08f88f0befb7eeb9afccdbdf6152a3e6f4d7c5d2bed5a9c17d"
+    url "https://github.com/aaronvb/request_hole/releases/download/v0.5.1/request_hole_0.5.1_Linux_arm64.tar.gz"
+    sha256 "4bdfcb68cfecce17c896cd293d6acc2e9a131f814424d04ebff6bc80473f51e0"
   end
 
   depends_on "go" => :optional
